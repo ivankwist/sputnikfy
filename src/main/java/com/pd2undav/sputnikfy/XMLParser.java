bd44ad9f8a1f452ed9b978bfbbae2e9fe09f70a7
Ivan Kwist <ivankwist@hotmail.com.ar>
2020-06-01 20:05:04 -0300

    Fixed import issue

--- src/main/java/com/pd2undav/sputnikfy/XMLParser.java
+++ src/main/java/com/pd2undav/sputnikfy/XMLParser.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
