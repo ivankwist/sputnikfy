f48a7f070f9303bf2ae430b6fd2609f6607a5ddd
Ivan Kwist <ivankwist@hotmail.com.ar>
2020-06-13 20:54:42 -0300

    Added SputnikfyHelperTest

---
+++ src/test/java/com/pd2undav/sputnikfy/SputnikfyHelperTest.java
@@ -0,0 +1,27 @@
+package com.pd2undav.sputnikfy;
+
+import org.apache.commons.io.FilenameUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SputnikfyHelperTest {
+
+    @Test
+    void testConvert() throws IOException {
+        MockMultipartFile testMultiPartFile = new MockMultipartFile("file",
+                "file.xml",
+                "application/xml",
+                new FileInputStream(new File("resources/actividad_valid.xml")));
+        File testFile = SputnikfyHelper.convert(testMultiPartFile);
+
+        assertTrue(testFile.exists());
+        assertNotNull(testFile.length());
+        assertEquals(testMultiPartFile.getName(), FilenameUtils.getBaseName(testFile.getName()));
+    }
+}
