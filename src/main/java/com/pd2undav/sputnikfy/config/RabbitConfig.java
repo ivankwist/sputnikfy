ec8f9530183fb2f4d3bdf4317e754fd46485bce4
Ivan Kwist <ivankwist@hotmail.com.ar>
2020-07-04 19:09:15 -0300

    RabbitMQ Config

--- src/main/java/com/pd2undav/sputnikfy/config/RabbitConfig.java
+++ src/main/java/com/pd2undav/sputnikfy/config/RabbitConfig.java
@@ -1,5 +1,8 @@
 package com.pd2undav.sputnikfy.config;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.*;
