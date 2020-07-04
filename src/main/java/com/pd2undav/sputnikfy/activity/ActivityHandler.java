2aa00f75edd133c57bd44511f07bc4c6b9a86296
Ivan Kwist <ivankwist@hotmail.com.ar>
2020-07-04 19:15:05 -0300

    Modified Rabbit Message

--- src/main/java/com/pd2undav/sputnikfy/activity/ActivityHandler.java
+++ src/main/java/com/pd2undav/sputnikfy/activity/ActivityHandler.java
@@ -4,6 +4,9 @@ import com.pd2undav.sputnikfy.model.ActivityMessage;
 import com.pd2undav.sputnikfy.xml.XMLParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.stereotype.Service;
@@ -29,7 +32,13 @@ public class ActivityHandler {
         List<ActivityMessage> activityMessages = this.XMLParser.parseXML(file);
 
         for (ActivityMessage activityMessage:activityMessages) {
-            rabbitTemplate.convertAndSend(SPUTNIKFY_EXCHANGE, activityMessage.getTopic(), activityMessage.getMessage());
+
+            Message message = MessageBuilder
+                    .withBody(activityMessage.getMessage().getBytes())
+                    .setContentType(MessageProperties.CONTENT_TYPE_JSON)
+                    .build();
+
+            rabbitTemplate.convertAndSend(SPUTNIKFY_EXCHANGE, activityMessage.getTopic(), message);
             logger.info("Sent message with routing-key '"+activityMessage.getTopic()+"': "+activityMessage.getMessage());
         }
     }
