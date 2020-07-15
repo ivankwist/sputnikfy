5c3ac6cb8b499a11121c9c5fa7a4affcaf48e508
Ivan Kwist <ivankwist@hotmail.com.ar>
2020-07-15 17:18:09 -0300

    Modified XMLUploadControllerTest

--- src/test/java/com/pd2undav/sputnikfy/xml/XMLUploadControllerTest.java
+++ src/test/java/com/pd2undav/sputnikfy/xml/XMLUploadControllerTest.java
@@ -1,9 +1,11 @@
 package com.pd2undav.sputnikfy.xml;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +24,9 @@ class XMLUploadControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@MockBean
+	private RabbitTemplate rabbitTemplate;
 
 	@Test
 	public void testValidXMLFileUpload() throws Exception {
